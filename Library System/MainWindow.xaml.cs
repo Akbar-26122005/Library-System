71865f9995ede2a056ea66472e912e63ead2e6dc
Akbar-26122005 <138172352+Akbar-26122005@users.noreply.github.com>
2025-02-19 03:57:02 +0500

    Update MainWindow.xaml.cs

--- Library System/MainWindow.xaml.cs
+++ Library System/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -53,15 +53,137 @@ namespace Library_System {
             }
 
             this.readers = this.context.readers.ToList();
-            for (int i = 0; i < 2; i++) {
-                Reader reader1 = this.context.readers.FirstOrDefault(r => r.readerId == i),
-                       reader2 = this.context.readers.FirstOrDefault(r => r.readerId == 5 - i);
-                (this.readers[i], this.readers[5 - i]) = (this.readers[5 - i], this.readers[i]);
-            }
+
+
+
+            //// Заполнение таблиц
+            //Random random = new Random();
+
+            //// Readers
+            //for (int i = 0; i < 500; i++) {
+            //    this.context.readers.Add(new Reader {
+            //        firstName = $"readerFirstName_{i + 1}",
+            //        lastName = $"readerLastName_{i + 1}",
+            //        email = $"readerEmail{i + 1}@gmail.com",
+            //        phoneNumber = (89998887766 - i).ToString(),
+            //        registrationDate = DateTime.Now.AddDays(i),
+            //        membershipStatus = "reader",
+            //    });
+            //}
+
+            //// Authors
+            //string[] nationalities = { "русский", "американец", "немец", "японец", "китаец", "чэчэнец" };
+            //for (int i = 0; i < 100; i++) {
+            //    int year = random.Next(1800, 2000),
+            //        month = random.Next(1, 13),
+            //        day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
+            //    Author author = new Author {
+            //        firstName = $"authorFirstName_{i + 1}",
+            //        lastName = $"authorLastName_{i + 1}",
+            //        dateOfBirth = new DateTime(year, month, day),
+            //        nationality = nationalities[random.Next(nationalities.Length)],
+            //        biography = "Писатель как писатель",
+            //        createdAt = DateTime.Now.AddDays(i),
+            //        updatedAt = DateTime.Now.AddDays(i),
+            //    };
+            //    this.context.authors.Add(author);
+            //}
+
+            //// Genres
+            //string[] genreNames = { "Любовные романы", "Классика", "Детектив", "Научная фантастика",
+            //                "Историческая фантастика", "Фэнтези", "Ужасы", "Боевик", "Приключения", "Триллер" };
+
+            //foreach (var genreName in genreNames) {
+            //    this.context.genres.Add(new Genre { genreName = genreName });
+            //}
+
+            //// Books
+            //for (int i = 0; i < 1000; i++) {
+            //    int totalCopies = random.Next(1000, 10001);
+            //    Author author;
+            //    do {
+            //        int index = random.Next(1, 101);
+            //        author = this.context.authors.FirstOrDefault(a => a.authorId == index);
+            //    } while (author is null);
+
+            //    Genre genre;
+            //    do {
+            //        int index = random.Next(1, 11);
+            //        genre = this.context.genres.FirstOrDefault(g => g.genreId == index);
+            //    } while (genre is null);
+
+            //    Book book = new Book {
+            //        title = $"book_{i + 1}",
+            //        AuthorId = author.authorId,
+            //        GenreId = genre.genreId,
+            //        Genre = genre,
+            //        publicshedYear = random.Next(author.dateOfBirth.Year + 18, author.dateOfBirth.Year + random.Next(19, 100)),
+            //        totalCopies = totalCopies,
+            //        availableCopies = random.Next(totalCopies + 1),
+            //        createdAt = DateTime.Now.AddDays(i),
+            //        updatedAt = DateTime.Now.AddDays(i),
+            //    };
+            //    this.context.books.Add(book);
+            //}
+
+            //// BooksLoans
+            //for (int i = 0; i < 100; i++) {
+            //    Reader reader;
+            //    do {
+            //        int index = random.Next(1, 501);
+            //        reader = this.context.readers.FirstOrDefault(r => r.readerId == index);
+            //    } while (reader is null);
+
+            //    Book book;
+            //    do {
+            //        int index = random.Next(40, 1001);
+            //        book = this.context.books.FirstOrDefault(b => b.bookId == index);
+            //    } while (book is null);
+
+            //    BookLoan bookLoan = new BookLoan {
+            //        ReaderId = reader.readerId,
+            //        Reader = reader,
+            //        BookId = book.bookId,
+            //        Book = book,
+            //        loanDate = DateTime.Now.AddDays(i),
+            //        returnDate = DateTime.Now.AddDays(i + 7),
+            //        dueDate = DateTime.Now.AddDays(i + 14),
+            //        status = "в аренде"
+            //    };
+            //    this.context.bookLoans.Add(bookLoan);
+            //}
+
+            //// BooksReviews
+            //for (int i = 0; i < 100; i++) {
+            //    Reader reader;
+            //    do {
+            //        int index = random.Next(1, 501);
+            //        reader = this.context.readers.FirstOrDefault(r => r.readerId == index);
+            //    } while (reader is null);
+
+            //    Book book;
+            //    do {
+            //        int index = random.Next(40, 1001);
+            //        book = this.context.books.FirstOrDefault(b => b.bookId == index);
+            //    } while (book is null);
+
+            //    BookReview bookReview = new BookReview {
+            //        ReaderId = reader.readerId,
+            //        Reader = reader,
+            //        BookId = book.bookId,
+            //        Book = book,
+            //        rating = random.Next(1, 11),
+            //        reviewText = $"review_{i + 1}",
+            //        reviewDate = DateTime.Now.AddDays(i),
+            //    };
+            //    this.context.bookReviews.Add(bookReview);
+            //}
 
             // Установка данных
             this.dataGrid.ItemsSource = this.readers;
             this.selectedData = "readers";
+
+            //this.context.SaveChanges();
         }
 
         private void MainWindow_Activated(object sender, EventArgs e) {
@@ -312,7 +434,6 @@ namespace Library_System {
             catch { }
         }
 
-        // Метод фильтрации данных
         private void DataFiltered<T>(List<T> dataList, in List<T> conList, PropertyInfo property) {
             string value = textBox_Value.Text.ToString();
             dataList.Clear();
@@ -513,5 +634,7 @@ namespace Library_System {
                 list.AddRange(greater);
             }catch { }
         }
+
+        private void btnSortData_Click(object sender, RoutedEventArgs e) => this.contextMenuSortData.IsOpen = true;
     }
 }
